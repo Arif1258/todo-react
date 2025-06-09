68d7937b5b112e4d3d97f4e12cabfb8c892d8858
Sk Arif Ahmed <skarifahmed@Sks-MacBook-Air.local>
2025-06-09 10:32:07 +0530

    done

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default function Todo(){
-    let [todos,setTodos]=useState(["sample task"]);
+    let [todos,setTodos]=useState([{task:"sample task",id:uuidv4()}]);
     let [newtodo,setNewtodo]=useState("");
 
     let updatevalue=(event)=>{
@@ -9,10 +11,44 @@ export default function Todo(){
     };
 
     let addnewTask=()=>{
-        setTodos([...todos,newtodo]);
+        setTodos((prevTodos)=>{
+            return [...todos,{task:newtodo,id:uuidv4()}]});
         setNewtodo("");
 
     }
+    let deleteTodo=(id)=>{
+        setTodos(todos.filter((todo)=>todo.id!=id));
+        
+    }
+    let uppercaseAll= ()=>{
+        setTodos(
+            todos.map((todo)=>{
+                return{
+                    ...Todo,
+                    task:todo.task.toUpperCase(),
+                };
+            })
+        )
+    }
+    // let uppercaseOne = (id)=>{
+    //     setTodos((todos)=>{
+    //         todos.map((todo)=>{
+    //             if(todo.id == id){
+    //                 return{
+    //                     ...Todo,
+    //                     task:todo.task.toUpperCase(),
+    //                 };
+
+    //             }else{
+    //                 return todo;
+    //             }
+                
+                
+    //         })}
+    //     )
+        
+
+    // }
 
     return(
         <div>
@@ -26,9 +62,15 @@ export default function Todo(){
             <h3>Todo List</h3>
             <ul>
                 {todos.map((todos)=>(
-                    <li>{todos}</li>
+                    <li key={todos.id}>
+                        <span>{todos.task}</span>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={()=>deleteTodo(todos.id)}>delete</button>
+                        {/* <button onClick={uppercaseOne}>Uppercase</button> */}
+                    </li>
                 ))}
             </ul>
+            <button onClick={uppercaseAll}>Uppercase All</button>
         </div>
     )
 }
